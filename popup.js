72f70483263ae2d3a2d0262bd7aa040eb62c3b4d
tgcshta <annorhtht@gmail.com>
2015-10-03 22:47:16 +0900

    #9 Send request to API.

--- popup.js
+++ popup.js
@@ -1,4 +1,8 @@
 
+/**********************************************************************
+ * Common
+ */
+
 /**
  * Constants
  */
@@ -8,39 +12,57 @@ var ENTRY_POINT='http://atami.kikurage.xyz';
  * Initialize
  */
 window.addEventListener('load', function() {
-  document.querySelector('#button').onclick = function() {
-    getImages({"q": "keyword"});
-  };
+  document.querySelector('#button').addEventListener('click', function() {
+    var text = document.querySelector("#search").value;
+    getImages({"q": text});
+  });
 }, false);
 
+/**********************************************************************
+ * API
+ */
+
 /**
- * GET /image/serch With query
+ * GET /image/search With query
  */
 function getImages(query) {
-  core('image/search', query, "GET", function(images, error) {
-    var parent = document.querySelector('#parent');
-    removeAllChildren(parent);
-
-    for (var i = 0; i < images.length; i++) {
-      var child = document.createElement('img');
-
-      child.src = images[i];
-      child.classList.add('stampImage');
-
-      parent.appendChild(child);
-    }
-  });
+  core('image/search', query, "GET")
+    .then(function(json) {
+      var parent = document.querySelector('#parent');
+      removeAllChildren(parent);
+      appendCards(parent, json);
+    }).catch(function(err) {
+      console.error(err);
+    });
 }
 
 /**
  * Core of API
  */
-function core(path, query, method, callback) {
+function core(path, query, method) {
   var url = generateUrl(path, query);
+  return fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+}
 
-  // TODO: fetchの実装
+/**********************************************************************
+ * Util
+ */
 
-  callback(["http://e-village.main.jp/gazou/image_gazou/shun_0009.jpg", "http://e-village.main.jp/gazou/image_gazou/shun_0008.jpg"], null);
+/**
+ * Append cards to parent
+ */
+function appendCards(parent, children) {
+  for (var i = 0; i < children.length; i++) {
+    var child = document.createElement('img');
+
+    child.src = children[i];
+    child.classList.add('stampImage');
+
+    parent.appendChild(child);
+  }
 }
 
 /**
