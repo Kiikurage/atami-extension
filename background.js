a8b8281373226be68824babcf1674da3895ab09c
Kiikurage <y.kikura@gmail.com>
2015-10-04 13:49:26 +0900

    fix many bugs

--- background.js
+++ background.js
@@ -1,16 +1,18 @@
 console.log("Hello World from background.js");
 
-var ENTRY_POINT='http://atami.kikurage.xyz';
+var ENTRY_POINT = 'http://atami.kikurage.xyz';
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  getImages(request.q)
-    .then(function(json) {
-      chrome.tabs.sendMessage(sender.tab.id, json);
-    })
-    .catch(function(err) {
-      sendResponse({farewell: err});
-      console.error(err);
-    });
+	getImages(request.q)
+		.then(function(json) {
+			chrome.tabs.sendMessage(sender.tab.id, json);
+		})
+		.catch(function(err) {
+			sendResponse({
+				farewell: err
+			});
+			console.error(err);
+		});
 });
 
 /**********************************************************************
@@ -21,27 +23,29 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
  * GET /image/search With query
  */
 function getImages(query) {
-  return core('image/search', query, "GET")
-    .then(function(json) {
-      return json;
-    });
+	return core('image/search', query, "GET")
+		.then(function(json) {
+			return json;
+		});
 }
 
 /**
  * Core of API
  */
 function core(path, query, method) {
-  var url = generateUrl(path, query);
-  return fetch(url).then(function(res) {
-    return res.json();
-  })
+	var url = generateUrl(path, {
+		q: query
+	});
+	return fetch(url).then(function(res) {
+		return res.json();
+	})
 }
 
 /**
  * Generate full url
  */
 function generateUrl(path, query) {
-  return ENTRY_POINT + '/' + path + '?' + Object.keys(query).map(function(key){
-      return key + '=' + encodeURIComponent(query[key]);
-    }).join('&');
+	return ENTRY_POINT + '/' + path + '?' + Object.keys(query).map(function(key) {
+		return key + '=' + encodeURIComponent(query[key]);
+	}).join('&');
 }
--- content_scripts.js
+++ content_scripts.js
@@ -1,3 +1,5 @@
+/* global $ chrome Clipboard */
+'use strict';
 
 /**********************************************************************
  * Common
@@ -6,43 +8,82 @@
 /**
  * Constants
  */
-var $base = null;
+var $base = null,
+	$search = null;
 var focusedElement = null;
 
+
 function init() {
-  window.addEventListener('keydown', function(e) {
-    if (e.shiftKey) {
-      if (e.keyCode === 68) {
-        focusedElement = document.activeElement;
-        $base.css({'top': $(focusedElement).offset().top+$(focusedElement).height(), 'left': $(focusedElement).offset().left});
-        $base.appendTo(document.body);
-        setupListener();
-      }
-    }
-  }, false);
-  loadHTML();
-};
+	window.addEventListener('keydown', function(ev) {
+		if (ev.shiftKey) {
+			if (ev.keyCode !== 68) return;
+			ev.preventDefault();
+
+			focusedElement = document.activeElement;
+
+			var $focusedElement = $(document.activeElement),
+				offset = $focusedElement.offset(),
+				top = offset.top,
+				left = offset.left,
+				bottom = window.innerHeight - top,
+				right = window.innerWidth - left;
+
+			if (top > bottom) {
+				$base.css({
+					'top': '',
+					'bottom': bottom + $focusedElement.height()
+				});
+			} else {
+				$base.css({
+					'top': top + $focusedElement.height(),
+					'bottom': ''
+				});
+			}
+
+			if (left > right) {
+				$base.css({
+					'left': '',
+					'right': right
+				});
+			} else {
+				$base.css({
+					'left': left,
+					'right': ''
+				});
+			}
+			$base.appendTo(document.body);
+			$search.focus();
+			setupListener();
+		}
+	}, false);
+	loadHTML();
+}
 
 function loadHTML() {
-  $.get(chrome.extension.getURL('form.html'))
-    .done(function(html){
-      $base = $(html);
-    });
-};
+	$.get(chrome.extension.getURL('form.html'))
+		.done(function(html) {
+			$base = $(html);
+			$search = $base.find('#atami-search');
+		});
+}
 
 function setupListener() {
-  new Clipboard('.stampImage');
-  document.querySelector('#atami-button').addEventListener('click', function() {
-    var text = document.querySelector("#atami-search").value;
-    // getImages({"q": text});
-    chrome.runtime.sendMessage({'q': text});
-  });
-};
+	new Clipboard('.stampImage');
 
-chrome.runtime.onMessage.addListener(function(json, sender, sendResponse) {
-  var parent = document.querySelector('#atami-parent');
-  removeAllChildren(parent);
-  appendCards(parent, json);
+	document.querySelector('#atami-form').addEventListener('submit', function(ev) {
+		var text = document.querySelector('#atami-search').value;
+		ev.preventDefault();
+
+		chrome.runtime.sendMessage({
+			'q': text
+		});
+	});
+}
+
+chrome.runtime.onMessage.addListener(function(json) {
+	var parent = document.querySelector('#atami-parent');
+	removeAllChildren(parent);
+	appendCards(parent, json);
 });
 
 $(init);
@@ -55,26 +96,28 @@ $(init);
  * Append cards to parent
  */
 function appendCards(parent, children) {
-  for (var i = 0; i < children.length; i++) {
-    var child = document.createElement('img');
+	var listener = function() {
+		focusedElement.value = this.src;
+		focusedElement.focus();
+		$base.remove();
+	};
 
-    child.classList.add('stampImage');
-    child.src = children[i]["proxiedUrl"];
-    child.dataset.clipboardText = children[i]["proxiedUrl"];
-    child.setAttribute('tabindex', 0);
-    child.addEventListener('click', function() {
-      focusedElement.value = this.src;
-      focusedElement.focus();
-      $base.remove();
-    });
+	for (var i = 0; i < children.length; i++) {
+		var child = document.createElement('img');
 
-    parent.appendChild(child);
-  }
+		child.classList.add('stampImage');
+		child.src = children[i].proxiedUrl;
+		child.dataset.clipboardText = children[i].proxiedUrl;
+		child.setAttribute('tabindex', 0);
+		child.addEventListener('click', listener);
+
+		parent.appendChild(child);
+	}
 }
 
 /**
  * Clear all elements of parent
  */
 function removeAllChildren(parent) {
-  parent.innerHTML = '';
+	parent.innerHTML = '';
 }
